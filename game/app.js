1e506029d1e90be26904bee59f6ba0046853872d
ahmad <1ahmadhazem@gmail.com>
2023-02-04 01:56:56 +0300

    basic particle class

--- game/app.js
+++ game/app.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
-let direction = null, PLAYER = null;
+let direction = null, PLAYER = null, PARTICLE;
 
 addEventListener("load", init)
 addEventListener("adjust_can", adjust_can)
@@ -35,21 +35,31 @@ function init()
  
     let width = 30
     let height = 30
-    let position = {x: canvas.width/2 - width, y: canvas.height/2 + height}
-    let color = "red"
+    let player_position = {x: canvas.width/2 - width, y: canvas.height/2 + height}
+    let player_color = "red"
     let velocity = 2
 
-    PLAYER = new player(position, width,height, velocity, color, canvas)
+    let particle_color = "white"
+    let raduis = 10
+
+    // get a position where the x and y aren't outside the canvas
+    let particle_position = {x: Math.random() * ((canvas.width - raduis) - raduis) + raduis,
+                             y: Math.random() * ((canvas.height - raduis) - raduis) + raduis}
+
+    PLAYER = new player(player_position, width, height, velocity, player_color, canvas)    
+    PARTICLE = new particle(raduis, particle_position, particle_color, ctx)
+
     animate()
 }
 
 function animate()
 {
     requestAnimationFrame(animate)
-    
-    ctx.fillStyle = "rgba(10, 10, 10, .1)"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+    ctx.fillStyle = "rgba(10, 10, 10, .5)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    
+    PARTICLE.update()
     PLAYER.update(direction)
 }
 
@@ -63,13 +73,13 @@ class player
         this.color = color;
         this.velocity = velocity;
         this.canvas = canvas;
-        this.ctx = canvas.ctx;
+        this.ctx = canvas.getContext("2d");
     }
 
     draw()
     {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
     update(direction)
@@ -83,6 +93,33 @@ class player
             this.position.y -= this.velocity;
         if(direction == 'D' && this.position.y + this.height <= this.canvas.height)
             this.position.y += this.velocity;
+
+        this.draw()
+    }
+}
+
+class particle
+{
+    constructor(raduis, position, color, ctx)
+    {
+        this.raduis = raduis;
+        this.position = position;
+        this.color = color;
+        this.ctx = ctx;
+        this.hue = 0;
+    }
+
+    draw()
+    {
+        this.ctx.fillStyle = this.color;
+        this.ctx.arc(this.position.x, this.position.y, this.raduis, 0, Math.PI * 2);
+        this.ctx.fill()
+    }
+ 
+    update()
+    {
+        this.hue++;
+        this.color = `hsl(${this.hue}, 70%, 70%)`
 
         this.draw()
     }
